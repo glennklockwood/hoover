1c6b1a77823b162f38a8528d8a7130f589c8bf85
Glenn K. Lockwood <glock@lbl.gov>
2016-10-07 15:34:17 -0700

    cosmetic changes

--- producer.c
+++ producer.c
@@ -13,6 +13,11 @@ int main(int argc, char **argv) {
     struct hoover_comm_config *config;
     struct hoover_tube *tube;
 
+    if ( argc < 2 ) {
+        fprintf( stderr, "Syntax: %s <file name> [file name [file name [...]]]\n", argv[0] );
+        return 1;
+    }
+
     /* Load the tube configuration  */
     if ( !(config = read_comm_config()) ) {
         fprintf( stderr, "NULL config\n" );
@@ -28,12 +33,7 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    /* Load file into an hoover data object */
-    if ( argc < 2 ) {
-        fprintf( stderr, "Syntax: %s <file name> [file name [file name [...]]]\n", argv[0] );
-        return 1;
-    }
-
+    /* Load files in as hoover data objects (HDOs) */
     char **filenames = &argv[1];
     uint32_t num_files = argc - 1;
     uint32_t num_headers = 0;
@@ -77,8 +77,10 @@ int main(int argc, char **argv) {
 
     /* build the manifest */
     char *manifest = build_manifest(headers, num_files);
+
     /* turn manifest into HDO */
     struct hoover_data_obj *manifest_hdo = manifest_to_hdo(manifest, strlen(manifest));
+
     /* create manifest header - first figure out what it should be called */
     char *manifest_fn_template = "manifest_%s_%s.json";
     size_t manifest_fn_len = sizeof(char)*(strlen(manifest_fn_template) + HOST_NAME_MAX + SHA_DIGEST_LENGTH_HEX + 1);
@@ -93,6 +95,7 @@ int main(int argc, char **argv) {
         gethostname(hostname, HOST_NAME_MAX);
         snprintf(manifest_fn, manifest_fn_len, manifest_fn_template, manifest_hdo->hash, hostname);
     }
+
     /* then build the manifest HDO's header */
     struct hoover_header *manifest_header = build_hoover_header(manifest_fn, manifest_hdo);
 
